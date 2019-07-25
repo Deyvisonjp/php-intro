e4755e6dc74ad1f18b946246be71fec1179f2b43
deyvisonjp <deyvisonjp@id.uff.br>
2019-07-25 15:20:14 -0300

    Criacao do index.php

--- script.php
+++ script.php
@@ -1,14 +1,56 @@
-<?php 
+<?php
 
-$categorias = array('infantil', 'adolescentes','adulto');
-    print_r($categorias);
-
-    echo("<br/>");
+$catetorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-defineCategoriaCompetidor($nome, $idade);
-header('location: index.php');
+/*
+var_dump($nome);
+var_dump($idade);
+*/
 
-?>
+/*
+if (empty($nome)) {
+    echo 'O nome não pode ser vazio';
+    return;
+}
+
+if (strlen($nome) < 3) {
+    echo 'O nome deve conter mais de 3 caracteres';
+    return;
+}
+
+if (strlen($nome) > 40) {
+    echo "O nome é muito extenso";
+    return;
+}
+
+if (!is_numeric($idade)) {
+    echo "Informe um número para idade";
+    return;
+}
+*/
+
+if ($idade >= 6 && $idade <= 12) {
+    for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'infantil')
+            echo "O nadador " . $nome . " compete na categoria " . $categorias[$i];
+    }
+
+} else if ($idade >= 13 && $idade <= 18) {
+    for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'adolescente')
+            echo "O nadador " . $nome . " compete na categoria adolescente";
+    }
+
+} else {
+    for ($i = 0; $i < count($categorias); $i++) {
+        if ($categorias[$i] == 'adulto')
+            echo "O nadador " . $nome . " compete na categoria adulto";
+    }
+
+}
