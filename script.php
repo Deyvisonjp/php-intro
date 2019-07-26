93124ed1dc18c7b0e2e7f1dd6d4f6de02f7e7b45
deyvisonjp <deyvisonjp@id.uff.br>
2019-07-26 10:46:52 -0300

    Adicionando validação de campos

--- script.php
+++ script.php
@@ -13,12 +13,27 @@ var_dump($nome);
 var_dump($idade);
 */
 
-/*
+//VALIDAÇÔES PARA NOME
 if (empty($nome)) {
     echo 'O nome não pode ser vazio';
     return;
 }
+if (strlen($nome) < 3){
+    echo 'O nome deve conter mais de 3 caracteres';
+    return;
+}
+if (strlen($nome) > 40){
+    echo '<h3>O nome não pode ser tão grande </h3>';
+    return;
+}
 
+//VALIDAÇÔES PARA IDADE
+if(!is_numeric($idade)){
+    echo "<h3>Informe um NUMERAL para idade!</h3>";
+    return;
+}
+
+/*
 if (strlen($nome) < 3) {
     echo 'O nome deve conter mais de 3 caracteres';
     return;
